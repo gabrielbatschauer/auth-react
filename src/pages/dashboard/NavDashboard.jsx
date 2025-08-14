8986b1da71643abbffe682cd433114691e203024
Gabriel <gbatschauer@gmail.com>
2025-08-14 15:42:23 -0300

    bugs: add imports

--- src/pages/dashboard/NavDashboard.jsx
+++ src/pages/dashboard/NavDashboard.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../../context/auth";
+import Spinner from "../../components/spinner/Spinner";
 
 function NavDashboard() {
   const { user, loading: isLoading, signOut } = useContext(AuthContext);
